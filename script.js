c60bec943a92469211467e5d93bcaa5e85c2b534
Tom Hoffmann <hoffmann1226@gmail.com>
2020-06-20 17:38:24 -0500

    submit button click event works

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ $(document).ready(init)
 
 function init(){
     console.log('in jq')
+    
+
+
+    //setup button click event handler
+    $('#submitBtn').on('click', gatherInput)
+    
+}
+
+function gatherInput(){
+    console.log('click works')
+    event.preventDefault();
 }
