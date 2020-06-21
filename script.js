10ba45f19853e17eaf30faf4017073e45cc00089
Tom Hoffmann <hoffmann1226@gmail.com>
2020-06-21 08:22:09 -0500

    object created in js

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ $(document).ready(init)
 
 function init(){
     console.log('in jq')
-    
-
-
     //setup button click event handler
     $('#submitBtn').on('click', gatherInput)
-    
 }
 
 function gatherInput(){
+    //verify that click works
     console.log('click works')
     event.preventDefault();
+
+
+    //create object to manipulate
+    let person = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#idNumber').val(),
+        jobTitle: $('#jobTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+    
 }
